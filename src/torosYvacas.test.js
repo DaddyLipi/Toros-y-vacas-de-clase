d3eaaa17d28d73cb0498a1f18fe6e10713902d6c
Jose Manuel Sanchez <piopiotrio@gmail.com>
2022-04-25 21:54:56 -0400

    Prueba roja, test 1

---
+++ src/torosYvacas.test.js
@@ -0,0 +1,6 @@
+describe("Toros y vacas ->  ganar", () => {
+    it("Si se adivino la cadena devolver ganaste", ()=>{
+        const torosyvacas = new TorosVacas("3");
+        expect(torosyvacas.adivinar("3")).toEqual("ganaste");
+    });
+});
